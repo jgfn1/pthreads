ecdebc7db4164799e4dbedafa6bc19be81242958
Victor Aurélio <ovictoraurelio@gmail.com>
2017-06-08 17:26:22 -0300

    Error in pthread_create

--- q6/q6.cpp
+++ q6/q6.cpp
@@ -4,7 +4,7 @@
 	* @since 13/11/16
 	*
 */
-#include <bits/stdc++.h>
+#include "stdc++.h"
 
 #define MAX_QUEUE_SIZE 5
 #define MAX_LUGGAGE 10
@@ -18,17 +18,25 @@ using namespace std;
 // TO EXECUTE:
 	// ./e < in
 
-
 // Queue Structures and ....
-  typedef struct Queue{
-	  int id;
-	  int size;
-	  pthread_mutex_t mtxCanUseQueue;
-	  pthread_cond_t cndQueueEmpty, cndQueueFill;
-  } Queue ;
+typedef struct queue{
+  int id;
+  int size;
+  pthread_mutex_t mtxCanUseQueue;
+  pthread_cond_t cndQueueEmpty, cndQueueFill;
+} Queue ;
 
-priority_queue<Queue> AirportQueuesInOrder;
-vector<Queue> AirportQueues; // each index is ID of sensor.
+class CompareQueue {
+	public:
+      bool operator() (Queue *a, Queue *b){
+  		    return a->size > b->size;
+  	  }
+};
+
+
+
+vector<Queue*> AirportQueues; // each index is ID of sensor.
+priority_queue<Queue*, vector<Queue*>, CompareQueue> AirportQueuesInOrder;
 pthread_mutex_t mtxAirportQueues;
 
 
@@ -40,25 +48,42 @@ pthread_cond_t bufferFill, bufferEmpty;
 // Variáveis do sensor
 int amountEnableSensors;
 
-void lock(pthread_mutex_t * mutexAddr){
-	pthread_mutex_lock(mutexAddr);
-}
+// Variáveis para manutenção
+//pthread_barrier_t barrierMaintenance;
 
-void unlock(pthread_mutex_t * mutexAddr){
-	pthread_mutex_unlock(mutexAddr);
-}
+void lock(pthread_mutex_t * mutexAddr){ pthread_mutex_lock(mutexAddr); }
+void unlock(pthread_mutex_t * mutexAddr){ pthread_mutex_unlock(mutexAddr); }
+void condWait(pthread_cond_t * condAddr, pthread_mutex_t * mutexAddr){ pthread_cond_wait(condAddr, mutexAddr); }
+void signal(pthread_cond_t *condAddr){ pthread_cond_signal(condAddr); }
+bool tryLock(pthread_mutex_t * mutexAddr){ return pthread_mutex_trylock(mutexAddr) == 0; }
+//void barrierWait(pthread_barrier_t * barrierAddr){ pthread_barrier_wait(barrierAddr); }
+void* track();
+void* sensor(int id);
+void putInQueue(Queue *q, int elements);
+Queue * getQueue();
 
-void condWait(pthread_cond_t * condAddr, pthread_mutex_t * mutexAddr){
-	pthread_cond_wait(condAddr, mutexAddr);
-}
 
-void barrierWait(pthread_barrier_t * barrierAddr){
-	pthread_barrier_wait(barrierAddr);
-}
+
 
 int main(){
 	// Allocating one thread for each line of matrix
-	vector< thread > ;
+  int nt, ns;
+	vector< pthread_t > ttracks;
+  vector< pthread_t > tsensors;
+  cin >> nt >> ns;
+
+  for(int i=0; i<ns; i++){
+      pthread_t tmp;
+      if(pthread_create(&tmp, NULL,sensor, i) == 0)
+          tsensors.push_back(tmp);
+  }
+  for(int i=0; i<nt; i++) {
+      pthread_t tmp;
+      if(pthread_create(&tmp, NULL,track,NULL) == 0)
+          ttracks.push_back(tmp);
+  }
+
+  pthread_exit(NULL);
 }
 
 /***
@@ -70,12 +95,12 @@ int main(){
 ***/
 void* track(){
 
-	Queue shortQueue = getQueue();
+	Queue *shortQueue = getQueue();
 
-	lock(shortQueue->mtxCanUseQueue);
+	lock(&shortQueue->mtxCanUseQueue);
 
 	while(shortQueue->size == MAX_QUEUE_SIZE){
-		wait(shortQueue->cndQueueEmpty);
+		condWait(&shortQueue->cndQueueEmpty, &shortQueue->mtxCanUseQueue);
 	}
 
 
@@ -96,30 +121,30 @@ void* sensor(int id){
 	while(true){
 		Queue *myQueue = AirportQueues[id];
 
-		lock(myQueue->mtxCanUseQueue);
+		lock(&myQueue->mtxCanUseQueue);
 		while(myQueue->size == 0){
-			signal(myQueue->mtxQueueEmpty); // Acordará alguma esteira se estiver bloqueada para escrever na queue...
-			pthread_cond_wait(myQueue->mtxQueueFill); // Sempre que alguma esteira escrever na queue ela dará siganl para o mtxQueueFIlll dentro da fila
+			signal(&myQueue->cndQueueEmpty); // Acordará alguma esteira se estiver bloqueada para escrever na queue...
+			condWait(&myQueue->cndQueueFill, &myQueue->mtxCanUseQueue); // Sempre que alguma esteira escrever na queue ela dará siganl para o mtxQueueFIlll dentro da fila
 		}
 
 		// Ler uma Mala
 		myQueue->size --;
-		sleep(0.5);
+		usleep(500);
 
 		// Se liga na porra da manutenção...
-		if(counterExecution == MAX_LUGGAGE){
+		if(amountLuggagePassed == MAX_LUGGAGE){
 			Queue *t;
-			if(t = getQueue() && t->id == -1){
+			if(t = getQueue(), t->id == -1){
 				putInQueue(myQueue, myQueue->size);
 				// Será que vai caber tudo? tenho que mandar um por um ensse carai
 			}
 
 			amountEnableSensors--;
-			barrierWait(A_BARREIRA_DE_MANUTENCAO);
+			//barrierWait(A_BARREIRA_DE_MANUTENCAO);
 			amountEnableSensors++;
 			amountLuggagePassed = 0;
 		}else{
-			unlock(myQueue->mtxCanUseQueue); // Para que minha fila já possa ser usada por alguma esteira..
+			unlock(&myQueue->mtxCanUseQueue); // Para que minha fila já possa ser usada por alguma esteira..
 		}
 	}
 	pthread_exit(NULL);
@@ -137,41 +162,47 @@ void* sensor(int id){
 		}
 */
 Queue * getQueue(){
-	lock(mtxAirportQueues);
+	lock(&mtxAirportQueues);
 
-	priority_queue<Queue> tmp;
+	vector<Queue*> tmp;
 	Queue *q, *queueTMP;
-	for(int i=0; i<AirportQueues.size() && lugage->id == -1; i++){
-		queueTMP = AirportQueues.front();
-		// try lock AirportQueues.front(){
-			// queue = AirportQueues.front();
-		//}
-		tmp.push(queueTMP);
-		AirportQueues.pop();
+  q = NULL;
+  queueTMP = AirportQueuesInOrder.top();
+	for(int i=0; i<AirportQueuesInOrder.size() && q == NULL; i++){
+		queueTMP = AirportQueuesInOrder.top();
+    if(tryLock(&queueTMP->mtxCanUseQueue)){
+        // mutex has been locked..
+        q = queueTMP;
+    }
+		tmp.push_back(queueTMP);
+		AirportQueuesInOrder.pop();
 	}
 
-	unlock(mtxAirportQueues);
+  // "atualizando da pior forma a queues in order"
+  while(!tmp.empty()){
+      AirportQueuesInOrder.push(tmp.back());
+      tmp.pop_back();
+  }
+
+	unlock(&mtxAirportQueues);
 	return q;
 }
 
 void putInQueue(Queue *q, int elements){
-	lock(mtxAirportQueues);
-	lock(q->mtxQueue);
+	lock(&mtxAirportQueues);
+	lock(&q->mtxCanUseQueue);
 
 	q->size += elements;
-	// Refazendo a priority_queue;
-	priority_queue<Queue, AirportQueues, CompareQueue> AirportQueuesInOrder;
 
-	// Refazendo a priority_queue;
-	priority_queue<Queue, vector<Queue>, CompareQueue> AirportQueuesInOrder;
-	//AirportQueuesInOrder = t; //wtf is 't'?
+  // "atualizando da pior forma a queues in order"
+  /*Queue refresh;
+  refresh.size = -1;
+  AirportQueuesInOrder.push(&refresh);// Adding refresh
+  AirportQueuesInOrder.pop();// Will remove refresh, because resfresh is smallest size...
+*/
+  make_heap(const_cast<Queue**> (&AirportQueuesInOrder.top()), const_cast<Queue**>(&AirportQueuesInOrder.top()) + AirportQueuesInOrder.size(), CompareQueue());
 
-	unlock(q->mtxQueue);
-	unlock(mtxAirportQueues);
+	unlock(&q->mtxCanUseQueue);
+	unlock(&mtxAirportQueues);
 }
 
-class CompareQueue {
-	bool operator() (Queue *a, Queue *b){
-		return a->size > b->size;
-	}
-};
