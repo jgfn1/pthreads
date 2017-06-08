4c2ebe89ac5c6cfe6c252ba2c77b4cabf713764d
Victor Aurelio Guimaraes Silva <vags@ug2c02.windows.cin.ufpe.br>
2017-06-08 12:31:47 -0300

    created q6.cpp sketch.

---
+++ q6/q6.cpp
@@ -0,0 +1,184 @@
+/***
+	*
+	* @author victoraurelio.com
+	* @since 13/11/16
+	*
+*/
+#include <bits/stdc++.h>
+
+#define MAX_QUEUE_SIZE 5
+#define MAX_LUGGAGE 10
+
+using namespace std;
+
+// TO COMPILE:
+    // g++ q6.cpp -std=c++0x -lpthread -o e
+    // g++ q6.cpp -std=c++0x -pthread -o e
+
+// TO EXECUTE:
+    // ./e < in
+
+
+// Queue Structures and ....
+  typedef struct {
+      int id;
+      int size;
+      pthread_mutex_t mtxCanUseQueue;
+      pthread_cond_t cndQueueEmpty, cndQueueFill;
+  } Queue ;
+
+  priority_queue<queue> AirportQueuesInOrder;
+  vector<queue> AirportQueues; // each index is ID of sensor.
+  pthread_mutex_t mtxAirportQueues;
+
+
+// Variáveis do buffer de realocação
+int bufferRealloc;
+pthread_mutex_t mtxBufferRealloc;
+pthread_cond_t bufferFill, bufferEmpty;
+
+// Variáveis do sensor
+int amountEnableSensors;
+
+
+int main(){
+    // Allocating one thread for each line of matrix
+    vector< thread > clients;
+
+    cin >> maxPages;
+    cin >> clientsLength;
+
+    hasNewRequest = 0;
+    canWriteRequest = true;
+
+    thread s = thread(server);
+
+    for(int i=0; i<clientsLength; i++){
+	     clients.push_back( thread(client, i) );
+    }
+
+
+
+    for(int i=0; i<clientsLength; i++){
+	     clients[i].join();
+    }
+
+    s.join();
+
+    pthread_exit(NULL);
+}
+
+/***
+  *
+  * @function esteira/trilha
+  * @description
+  *    This function build pages and serve on buffer.
+  *
+***/
+void* track(){
+
+    Queue shortQueue = getQueue();
+
+    shortQueue->mtxCanUseQueue;
+    while(shortQueue->size == MAX_QUEUE_SIZE){
+        wait(shortQueue->cndQueueEmpty)
+    }
+
+
+    pthread_exit(NULL);
+}
+
+/***
+  *
+  * @function sensor
+  * @description
+  *    ...
+  *
+***/
+void* sensor(int id){
+    int amountLuggagePassed = 0;
+    while(true){
+        Queue *myQueue = AirportQueues[id];
+
+        lock(myQueue->mtxCanUseQueue);
+        while(myQueue->size == 0){
+            signal(myQueue->mtxQueueEmpty); // Acordará alguma esteira se estiver bloqueada para escrever na queue...
+            pthread_cond_wait(myQueue->mtxQueueFill); // Sempre que alguma esteira escrever na queue ela dará siganl para o mtxQueueFIlll dentro da fila
+        }
+
+        // Ler uma Mala
+        myQueue->size --;
+        sleep(0,5);
+
+
+        // COntando que o sensor consumiu mais uma mala
+        amountLuggagePassed++:
+
+        // Se liga na porra da manutenção...
+        if(counterExecution == MAX_LUGGAGE){
+            Queue *t;
+            if(t = getQueue() && t->id == -1){
+                putInQueue(myQueue, myQueue->size);
+                // Será que vai caber tudo? tenho que mandar um por um ensse carai
+            }
+
+            amountEnableSensors--;
+            pthread_cond_wait(A_BARREIRA_DE_MANUTENCAO);
+            amountEnableSensors++;
+            amountLuggagePassed = 0;
+        }else{
+            unlock(myQueue->mtxCanUseQueue); // Para que minha fila já possa ser usada por alguma esteira..
+        }
+    }
+    pthread_exit(NULL);
+}
+
+/*
+    Objetivo:
+        Consultar a disponibilidade de inserir uma mala em uma fila, dando prioridade as filas de menor tamanho.
+
+    Caminho:
+        Damos no lock na priority_queue de filas do aeroporto e retiramos item por item, até que seja possível dar lock na fila
+        if(foi possivel dar lock na fila){
+            Então retornaremos a fila e ela está locked ou seja em alguma execução paralela de
+                        getqueue ele nao conseguirá retirar ou inserir um item dessa fila.
+        }
+*/
+Mala* getQueue(){
+    lock(mtxAirportQueues);
+
+    priority_queue<Queue> tmp;
+    Queue *q, *queueTMP;
+    for(int i=0; i<AirportQueues.size() && lugage->id == -1; i++){
+        queueTMP = AirportQueues.front();
+        // try lock AirportQueues.front(){
+            // queue = AirportQueues.front();
+        //}
+        tmp.push(queueTMP);
+        AirportQueues.pop();
+    }
+
+    unlock(mtxAirportQueues);
+    return q;
+}
+
+void putInQueue(Queue *q, int elements){
+    lock(mtxAirportQueues);
+    lock(q->mtxQueue);
+
+    q->size += elements;
+    // Refazendo a priority_queue;
+    priority_queue<Queue, AirportQueues, CompareQueue> AirportQueuesInOrder;
+
+    // Refazendo a priority_queue;
+        priority_queue<Queue, vector<Queue>, CompareQueue> AirportQueuesInOrder;
+        AirportQueuesInOrder = t;
+    unlock(q->mtxQueue);
+    unlock(mtxAirportQueues);
+}
+
+class CompareQueue {
+    bool operator() (Queue *a, Queue *b){
+        return a->size > b->size;
+    }
+};
